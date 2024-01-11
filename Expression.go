6a3304f06d8feb8c523c1f605374df06c49b58da
kkoehler <Kristian.Koehler@source-fellows.com>
2024-01-11 09:58:03 +0100

    added RexexOption values

--- Expression.go
+++ Expression.go
@@ -35,6 +35,27 @@ type RegexpOption string
 // RegexpOptionCaseInsensitivity case insensitivity to match upper and lower cases.
 const RegexpOptionCaseInsensitivity = RegexpOption("i")
 
+// RegexpOptionMultiline is for patterns that include anchors
+// (i.e. ^ for the start, $ for the end), match at the
+// beginning or end of each line for strings with multiline values.
+// Without this option, these anchors match at beginning or end of the string.
+// For an example, see Multiline Match for Lines Starting with Specified Pattern.
+const RegexpOptionMultiline = RegexpOption("M")
+
+// RegexpOptionExtended defines "extended" capability to ignore all white space characters
+// in the pattern unless escaped or included in a character class.
+// Additionally, it ignores characters in-between and including an un-escaped
+// hash/pound (#) character and the next new line, so that you may include comments
+// in complicated patterns. This only applies to data characters;
+// white space characters may never appear within special character sequences in a pattern.
+//
+// The x option does not affect the handling of the VT character (i.e. code 11).
+const RegexpOptionExtended = RegexpOption("x")
+
+// RegexpOptionMatchAll allows the dot character (i.e. .) to match all characters including
+// newline characters.
+const RegexpOptionMatchAll = RegexpOption("s")
+
 // Field represents a single field in a BSON document.
 type Field string
 
